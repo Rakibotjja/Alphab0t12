606b3b48e289257062018cc0f069bee6bea63b0f
Rakibotjja <97228297+Rakibotjja@users.noreply.github.com>
2022-01-06 18:19:30 +0700

    Update zeeoneofc.js

--- zeeone/zeeoneofc.js
+++ zeeone/zeeoneofc.js
@@ -110,7 +110,7 @@ ${petik}└❒ ${prefix}anime${petik} _random_
 `
 }
 const allmenu = (prefix, wita, wit, ucapannya2, timuu, status, wa_version, mcc, mnc, os_version, device_manufacturer, device_model, alfa , alfa1, thisDay, ini_tanggal, totalchat, hit_today, ini_gcchat, latensiii, tekss)  => {
-	return`❏───「 *SANZ BOT* 」───❏
+	return`❏───「 *KHAFA BOT* 」───❏
      
 ┌❏ *INDONESIA TIME*
 ${petik}│◦➛ WIB : ${timuu}
@@ -891,7 +891,7 @@ ${petik}│◦ ${prefix}rikagusriani
 │
 └─❑「 MENU BOT 」
 
-❏───「 *SANZ BOT* 」───❏
+❏───「 *KHAFA BOT* 」───❏
 `
 }
 
